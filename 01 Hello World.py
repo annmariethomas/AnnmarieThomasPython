b85a341bd5e5459febfdf8bba606c62fcb97e9c3
annmariethomas <88806893+annmariethomas@users.noreply.github.com>
2021-10-24 12:08:30 +1100

    Add files via upload

---
+++ 01 Hello World.py
@@ -0,0 +1,6 @@
+print('--------------------------')
+print('|  01HelloWorld          |')
+print('|  Name: Annmarie Thomas        |')
+print('|  Version: 01           |')
+print('--------------------------')
+print('Hello, world!')
