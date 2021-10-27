1edaf196dcae071b30be9452d1390cfce6b1611a
annmariethomas <88806893+annmariethomas@users.noreply.github.com>
2021-10-27 11:54:52 +1100

    Add files via upload

---
+++ 05 For Loop.py
@@ -0,0 +1,9 @@
+print(" ------------------------------------------------") 
+print("|                                                |")
+print("|    05ForLoop                                   |")
+print("|    Name : Annmarie Thomas                      |") 
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+for x in range(1,500):
+  print(x)
